5df1dacd8b117cf26ff0c99802dbb0d4e495da7a
patrick <vyltyan@gmail.com>
2026-01-15 21:06:11 +0100

    Set DEBUG to False for production environment

--- bingo_rentals/settings.py
+++ bingo_rentals/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-8az%z&d4)2$+-!yh##q9=iw7$*^8eot4d73ha)bovj&u)2b$p=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = False
 
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
